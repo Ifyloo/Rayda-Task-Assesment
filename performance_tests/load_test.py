ff755ebadb3b8e7bdc8976a5352a2a1c9577d855
Ifyloo <ifyloo2001@gmail.com>
2025-06-29 20:24:23 +0100

    Update load_test.py

--- performance_tests/load_test.py
+++ performance_tests/load_test.py
@@ -1,1 +1,20 @@
+# load_test.py
+
+from locust import HttpUser, task, between
+
+class UserLoadTest(HttpUser):
+    wait_time = between(1, 3)
+    
+    @task
+    def get_users(self):
+        self.client.get("/api/users")
+
+    @task
+    def create_user(self):
+        payload = {
+            "username": "loaduser1",
+            "email": "loaduser1@example.com",
+            "password": "LoadPass123!"
+        }
+        self.client.post("/api/users", json=payload)
 
