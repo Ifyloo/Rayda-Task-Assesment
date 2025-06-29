f2ace5cab81b6c5f05a168018a4800fb4e87ac58
Ifyloo <ifyloo2001@gmail.com>
2025-06-29 20:25:50 +0100

    Update report_generators.py

--- utils/report_generators.py
+++ utils/report_generators.py
@@ -1,1 +1,8 @@
+# utils/report_generators.py
+
+def generate_test_report(results):
+    with open("test_report.txt", "w") as f:
+        f.write("Test Results:\n")
+        for result in results:
+            f.write(f"{result}\\n")
 
