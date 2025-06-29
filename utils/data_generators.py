4e76efd0f8d33e67c7a9dfded20b71ce96ee6bed
Ifyloo <ifyloo2001@gmail.com>
2025-06-29 20:25:32 +0100

    Update data_generators.py

--- utils/data_generators.py
+++ utils/data_generators.py
@@ -1,1 +1,11 @@
+# utils/data_generators.py
+
+import random
+import string
+
+def generate_random_username():
+    return "user_" + ''.join(random.choices(string.ascii_lowercase, k=6))
+
+def generate_random_email():
+    return generate_random_username() + "@example.com"
 
